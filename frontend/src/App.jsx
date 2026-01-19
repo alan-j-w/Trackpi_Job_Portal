1d260d2062feca65f9b96baca681b35e2a2530d1
hasna9275 <hasnakanakkote@gmail.com>
2026-01-19 14:38:14 +0530

    git merge conflict resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom
 /* Pages */
 import Home from "./pages/Home";
 /* Admin Pages */
-/* Admin Pages */
-import AdminLayout from "./layouts/AdminLayout";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import AdminJobs from "./pages/admin/AdminJobs";
-import AdminApplicants from "./pages/admin/AdminApplicants";
-import AdminManagement from "./pages/admin/AdminManagement";
-// Import new Admin pages as placeholders or actual if exist, for now just reuse or placeholders for routing
-import UserDashboard from "./pages/user/UserDashboard";
-import SuperAdminDashboard from "./pages/superadmin/SuperAdminDashboard";
+
 
 import ContactUs from "./pages/ContactUs";
 import Login from "./pages/Login";
@@ -22,9 +14,7 @@ import Profile from "./pages/Profile";
 
 /* Route Protection */
 import ProtectedRoute from "./components/ProtectedRoute";
-import ProtectedAdminRoute from "./components/ProtectedAdminRoute";
 import RedirectIfSuperAdmin from "./components/RedirectIfSuperAdmin";
-import { PERMISSIONS } from "./constants/permissions";
 
 import ResumeGen from "./pages/ResumeGen";
 import About from "./pages/About";
@@ -69,6 +59,7 @@ function App() {
         <Route path ="/talent-league" element={<TalentLeague />} />
         <Route path ="/creators" element={<Creators />} />
         
+        </Route>
       </Routes>
     </Router>
   );
