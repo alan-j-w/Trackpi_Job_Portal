449b2d0f740cb02750b570891a9be400d59767ea
adhy-k <adhykrishna123@gmail.com>
2026-02-20 12:43:11 +0530

    small change to actions in hiring partners

--- frontend/src/pages/admin/AdminHiringPartners.jsx
+++ frontend/src/pages/admin/AdminHiringPartners.jsx
@@ -158,8 +158,8 @@ const AdminHiringPartners = () => {
                                 <th className="p-4">Organization name</th>
                                 <th className="p-4 text-center">Logo</th>
                                 <th className="p-4 text-center">Email</th>
-                                <th className="p-4 w-1/3">About company</th>
-                                <th className="p-4 text-center">Action</th>
+                                <th className="p-4 w-1/3">About company</th>on 
+                                <th className="p-4 text-center">Actions</th>
                             </tr>
                         </thead>
                         <tbody className="divide-y divide-gray-100">
